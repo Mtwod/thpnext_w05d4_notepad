94bdcc8b23eda7566f78d8e5402ad129dd42cb3a
Mtwod <maximewong@hotmail.com>
2021-02-04 12:05:36 +0100

    Initial commit with starter setup, eslint & sass

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+    <p>This is an introduction, place your components here!</p>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
