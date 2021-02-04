88b1c8a731576eb15d9f358c3d2a60785ac0da77
Mtwod <maximewong@hotmail.com>
2021-02-04 16:07:31 +0100

    Set controlled component in MarkDown

--- src/components/MarkDownInput/index.jsx
+++ src/components/MarkDownInput/index.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import './index.scss';
 
-const MarkDownInput = () => {
-  const [title, setTitle] = React.useState('');
-  const [content, setContent] = React.useState('');
-
-  const handleChangeTitle = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const handleChangeContent = () => {
-    setContent(document.getElementsByClassName('MarkDownInput__content')[0].innerHTML);
-  };
-
+const MarkDownInput = ({ note, onChangeTitle, onChangeContent }) => {
   const handleClickSave = () => {
     console.log('You just clicked save!');
   };
 
   return (
     <div className="MarkDownInput">
-      <input className="MarkDownInput__title" value={title} type="text" onChange={handleChangeTitle} />
-      <textarea className="MarkDownInput__content" onChange={handleChangeContent}>
-        {content}
-      </textarea>
+      <input className="MarkDownInput__title" value={note.title} type="text" onChange={onChangeTitle} />
+      <textarea className="MarkDownInput__content" value={note.content} onChange={onChangeContent} />
       <button type="button" className="MarkDownInput__save" onClick={handleClickSave}>Sauvegarder</button>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 
-const App = () => (
-  <div className="App">
-    <NavBar />
-    <MarkDownInput />
-    <NoteDisplay />
-  </div>
-);
+// const localStorage = window.localStorage;
+
+const App = () => {
+  const [note, setNote] = React.useState({ title: '', content: '' });
+
+  const handleChangeTitle = (event) => {
+    setNote({ title: event.target.value });
+  };
+
+  const handleChangeContent = (event) => {
+    setNote({ content: event.target.value });
+  };
+
+  return (
+    <div className="App">
+      <NavBar />
+      <MarkDownInput
+        onChangeTitle={handleChangeTitle}
+        onChangeContent={handleChangeContent}
+        note={note}
+      />
+      <NoteDisplay />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
